3ca0fef385a4051808e69ca05efa2960ab582533
shashankyadava <78867250+shashankyadava@users.noreply.github.com>
2023-07-30 14:13:30 +0530

    Update UserRouter.js
    removed signup route

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,8 +1,7 @@
 import express from 'express';
-import  { signup ,login}  from '../controllers/userController.js';
+import  { login }  from '../controllers/userController.js';
 
 const UserRouter = express.Router();
-  UserRouter.post('/signup',signup);
   UserRouter.post('/login',login);
   export default UserRouter;
   
